bcb7f983ccd91491254e122ccceb3683fd0b5148
xGladiate <sharonsohxuanhui@hotmail.com>
2024-09-28 23:04:11 +0800

    Create a bot with basic features

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+        String botToken = "7803548047:AAG7cIPEdmLIOSEES5570yqG0qJxAmzZCto";
+        // Using try-with-resources to allow autoclose to run upon finishing
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new HealthcareBot(botToken));
+            System.out.println("MyAmazingBot successfully started!");
+            // Ensure this prcess wait forever
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
